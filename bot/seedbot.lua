a18b5dec837c338425d3f6fb6be9901b7fa83509
behradkaka <kakaeibehrad@gmail.com>
2016-12-16 11:58:01 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,15 +224,10 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
-    "admin"
-    },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber 302383135},--Sudo users
-    disabled_channels = {},
-    moderation = {data = 'data/moderation.json'},
+    "admin43723991,111020322,0,tonumber 302383135},--Sud
     about_text = [[Teleseed v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
+302383135https://github.com/SEEDTEAM/TeleSeed
 
 Our team!
 Alphonse (@Iwals)
